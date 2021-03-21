903656b3fdff8de243131ca647950fa924cd65ac
Vilas Kaware <vilaskaware1234@gmail.com>
2021-03-21 19:06:25 +0530

    minor changes in app and training

--- app.py
+++ app.py
@@ -17,18 +17,22 @@ def predict():
     inputs = [val for val in request.form.values()]
     result = model.predict([[inputs[0], inputs[1]]])
     pred = ''
-    if result[0] == 0:
-        pred = "You can invest in Equity market for long and short term as well as in bonds"
-    elif result[0] == 1:
-        pred = "You can invest in Equity market for long and short term"
-    elif result[0] == 2:
-        pred = "You can invest in Equity market, bonds and also trade in intraday"
-    elif result[0] == 3:
-        pred = "You can invest in Equity market, bonds and start intraday with strict stoploss"
-    elif result[0] == 4:
-        pred = "You can invest in Equity market for long term"
+    if int(inputs[0]) < int(18):
+        pred = "Please sit back and relax for few years"
     else:
-        pred = 'some error'
+        if result[0] == 0:
+            pred = "You can invest in Equity market for long and short term as well as in bonds"
+        elif result[0] == 1:
+            pred = "You can invest in Equity market for long and short term"
+        elif result[0] == 2:
+            pred = "You can invest in Equity market, bonds and also trade in intraday"
+        elif result[0] == 3:
+            pred = "You can invest in Equity market, bonds and start intraday with strict stoploss"
+        elif result[0] == 4:
+            pred = "You can invest in Equity market for long term"
+        else:
+            pred = 'some error'
+    print("Cluster ", result[0])
     return render_template('index.html', result=pred)
 
 
